6e8b55dab3518444b2cf7577b7ec2c15cee32945
egobabyr <putubrough@gmail.com>
2026-05-05 16:51:57 +1200

    added skeleton for game function, including functions and explaining their uses and stuff

--- server/game.js
+++ server/game.js
@@ -1,6 +1,7 @@
 //Game States
 export const SUITS  = ['♠', '♥', '♦', '♣'];
 export const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+export const cardGames = [LastCard];
 
 //Builds the deck
 export function buildDeck() {
@@ -56,4 +57,63 @@ export function createGame(playerIDS) {
     winner:    null,
     log:       'Game Started!'
   };
+}
+
+export function startGame(selectedGame) {
+  isGameValid = true; //Should current game keep going
+
+  if (selectedGame == LastCard) {
+    playerID = true //Paalav hmu what are the playerIDS
+    isCardPlayed == false;
+
+    while (isGameValid == true){
+      if (isCardPlayed == true){ //Wait for a card to be played
+        playTurn(selectedGame, playerID, cardPlayed)
+        playerID = !playerID
+      }
+    }
+
+  }
+  
+}
+
+//Logic for each player's turn in card game
+export function playTurn(selectedGame, playerID, cardPlayed) {
+  isPlayerTurn = 0; //Index of current player's turn - 0: player 1, 1: player 2
+
+  if(selectedGame == LastCard) {
+    if (playerID == true) { //PlayerID true = player 1
+
+    }
+    else { //PlayerID false = player 2                        ***BUT WILL CHANGE AS I LEARN WHAT PLAYERIDS WILL BE LIKE
+
+    }
+  }
+  
+
+}
+
+//Checks if the card a player has drawn works as turn
+export function isLegalPlay(cardPlayed) {
+  //if played card is a special card such as .... if not, just play card like normal
+}
+
+//If the played card is a special card such as ...., apply effects
+export function applyCardEffect(playerID, cardPlayed) {
+
+}
+
+//Draws card from pile, adds to player hand, then removes from pile
+export function drawCard(playerID) {
+
+}
+
+//Change next player. (able to add reverse card effects and such in future)
+export function nextPlayer(playerID) {
+
+}
+
+//Check if current player hand is empty. If empty, win
+export function checkWinner(playerID) {
+
 }
